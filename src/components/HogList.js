6e2db122d23988b684ea82bcea3a6bf512851523
+ <jkunins95@gmail.com>
2023-11-14 14:09:54 -0500

    completed deliverable 3, added filter button to show greased hogs

--- src/components/HogList.js
+++ src/components/HogList.js
@@ -1,30 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import hogs from "../porkers_data";
 
 import HogDetails from "./HogDetails";
 
 const HogList = () => {
+  const [showGreasedHogs, setShowGreasedHogs] = useState(false);
+
+  const toggleGreasedHogs = () => {
+    setShowGreasedHogs(!showGreasedHogs);
+  };
+
+  // Filter hogs (greased condition)
+  const filteredHogs = showGreasedHogs ? hogs.filter((hog) => hog.greased) : hogs;
+
   return ( 
-    <div className="ui grid container">
-      {hogs.map((hog) => (
-        <div className="ui eight wide column" key={hog.name}>
-          <div className="ui card">
-            <div className="image">
-              <img key={hog.name} src={hog.image} alt={hog.name} />
-            </div>
-            <div className="content">
-              <a className="header">{hog.name}</a>
-          <HogDetails
-            specialty={hog.specialty}
-            greased={hog.greased}
-            weight={hog.weight}
-            highestMedal={hog["highest medal achieved"]}
-          />
+    <>
+      <button onClick={toggleGreasedHogs}>
+        {showGreasedHogs ? 'Show All Hogs' : 'Show Greased Hogs'}
+      </button>
+      <div className="ui grid container">
+        {filteredHogs.map((hog) => (
+          <div className="ui eight wide column" key={hog.name}>
+            <div className="ui card">
+              <div className="image">
+                <img key={hog.name} src={hog.image} alt={hog.name} />
+              </div>
+              <div className="content">
+                <a className="header">{hog.name}</a>
+            <HogDetails
+              specialty={hog.specialty}
+              greased={hog.greased}
+              weight={hog.weight}
+              highestMedal={hog["highest medal achieved"]}
+            />
+              </div>
             </div>
           </div>
-        </div>
       ))}
-    </div>
+      </div>
+    </>
   );
 }
 
